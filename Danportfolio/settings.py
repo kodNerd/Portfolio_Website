33ef367427ff098f13f4b759242b734d80f3c80e
kodNerd <bencoder9@gmail.com>
2024-04-06 12:49:15 +0300

    debugging error 500

--- Danportfolio/settings.py
+++ Danportfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p!w1s8kejhc9^)o4hmr4)y@d&gct!-z#9%+!6*003=c(qru&7-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
