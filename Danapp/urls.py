139c23e87c45a0a0e4e417dd6d388dc4b13936ac
kodNerd <bencoder9@gmail.com>
2024-04-06 13:11:49 +0300

    added portfolio-details path

--- Danapp/urls.py
+++ Danapp/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 urlpatterns =[
     path('',views.index,name='index'),
-    path('about/',views.about,name='about')
+    path('about/',views.about,name='about'),
+    path('portfolio_details/',views.portfolio_details,name='portfolio_details')
 ]
