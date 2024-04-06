87225dedb26419a17947cfdf655165e0fdc16fbc
kodNerd <bencoder9@gmail.com>
2024-04-06 13:25:46 +0300

    removed about pattern

--- Danapp/urls.py
+++ Danapp/urls.py
@@ -2,6 +2,5 @@ from . import views
 from django.urls import path
 urlpatterns =[
     path('',views.index,name='index'),
-    path('about/',views.about,name='about'),
     path('portfolio_details/',views.portfolio_details,name='portfolio_details')
 ]
