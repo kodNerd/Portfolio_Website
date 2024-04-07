e9356b0f348a07079add8d1418eb0d5aefa9a304
kodNerd <bencoder9@gmail.com>
2024-04-07 22:01:05 +0300

    contact form

--- Danapp/urls.py
+++ Danapp/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 urlpatterns =[
     path('',views.index,name='index'),
-    path('portfolio_details/',views.portfolio_details,name='portfolio_details')
+    path('portfolio_details/',views.portfolio_details,name='portfolio_details'),
+    path('contact/',views.contact,name='contact'),
+    path('portfolio_view',views.portfolio_view,name='portfolio_view')
 ]
--- Danapp/views.py
+++ Danapp/views.py
@@ -5,56 +5,39 @@ from django.core.mail import send_mail
 from django.conf import settings
 # Create your views here.
 def index(request):
-    if request.method == "POST":
-        message = request.POST['message']
-        email = request.POST['email']
-        name = request.POST['name']
-        subject = request.POST['subject']
-        
-        send_mail(
-            subject,  
-            message,  
-            email,  
-            ['bencoder9@gmail.com'], 
-        )
     return render(request, 'Danapp/index.html')
 
-# def courses(request):
-#     return render(request,'Home/courses.html')
-# def events(request):
-#     return render(request,'Home/events.html')
-# def teacherProfile(request):
-#     return render(request,'Home/teacherProfile.html')
-# def blog(request):
-#     return render(request,'Home/blog.html')
-# def about(request):
-#     return render(request,'Danapp/about.html')
-# def login(request):
-#     return render(request,'Home/login.html')
-
-# def careers(request):
-#     return render(request, 'Home/careers.html')
-# def register(request):
-#     if request.method == 'POST':
-#         UserCred = request.POST['UserCred']
-#         password = request.POST['password']
-#         password2 = request.POST['password2']
-#         fname=request.POST['fname']
-#         if password2 == password:
-#             if User.objects.filter(UserCred=UserCred).exists():
-#                 messages.info(request,'User already registered')
-#                 return redirect('Home/register.html')
-#             if User.objects.filter(password=password).exists():
-#                 messages.info(request,'Password too easy!') 
-#                 return redirect('Home/register.html')
-#             else:
-#                 user = User.objects.create_user(password=password2,UserCred=UserCred) 
-#                 user.save()
-#                 return redirect('login.html')
-#         elif password != password2:
-#             messages.info(request,'Passwords did not match!')
-#         else:
-#             return redirect('Home/register.html')
-#     return render(request, 'Home/register.html')
+def portfolio_view(request):
+    # CONTACT FORM
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        form_data = {
+            'name':name,
+            'email':email,
+            'phone':phone,
+            'message':message,
+        }
+        message = '''
+        From:\n\t\t{}
+        Message:\n\t\t{}\n
+        Email:\n\t\t{}
+        Phone:\n\t\t{}\n
+        '''.format(form_data['name'], form_data['message'], form_data['email'],form_data['phone'])
+        send_mail('You got a mail Ben!', message, '', ['bencoder9@gmail.com']) # TODO: enter your email address
+        
+    return render(request, 'Danapp/success.html', {})
+def contact(request):
+    return render(request,'Danapp/contact.html')
 def portfolio_details(request):
     return render(request, 'Danapp/portfolio-details.html')
+def about(request):
+    return render(request,'Danapp/about.html')
+def portfolio(request):
+    return render(request,'Danapp/portfolio.html')
+def resume(request):
+    return render(request, 'Danapp/resume.html')
+def services(request):
+    return render(request, 'Danapp/services.html')
